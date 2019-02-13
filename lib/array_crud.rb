3f859f0bfdeb908e52848bf17f1e2a82a9744c49
Paul Rosero <paul_rosero@yahoo.com>
2019-02-13 00:54:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["w", "x", "y", "z", "arrays!"]
-  
+  arrays! = array.pop
+  puts arrays!
 end
 
 def remove_element_from_start_of_array(array)
